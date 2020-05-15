10e13ae793ccf5f52d125476a8882800ec9e822a
ClownMonster <mk6386223@gmail.com>
2020-05-15 20:40:37 +0530

    deployment ready

---
+++ build/precache-manifest.89f8af32e21f03470fe1d958b7f1b44c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7a0310592f04db12a3af",
+    "url": "/static/js/main.7a031059.chunk.js"
+  },
+  {
+    "revision": "f9b5a43d049f0135c8ae",
+    "url": "/static/js/1.f9b5a43d.chunk.js"
+  },
+  {
+    "revision": "7a0310592f04db12a3af",
+    "url": "/static/css/main.4a66c560.chunk.css"
+  },
+  {
+    "revision": "f9b5a43d049f0135c8ae",
+    "url": "/static/css/1.8426e1aa.chunk.css"
+  },
+  {
+    "revision": "c01aca6eb91e8d690b06207ea1580c92",
+    "url": "/index.html"
+  }
+];
